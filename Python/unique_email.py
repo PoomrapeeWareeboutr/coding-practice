9bb4c019d98ab7e9ac00a47e587d47376f7826fe
pw <100401430+PoomrapeeWareeboutr@users.noreply.github.com>
2022-03-12 09:19:25 +0700

    LeetCode #929 - Unique Email Addresses

---
+++ Python/unique_email.py
@@ -0,0 +1,35 @@
+class Solution:
+
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        i = 0
+        j = 0
+        emails_set = set()
+        edit_string = []
+        str_temp = ""
+        index = 0
+        for i in emails:
+            str_temp = ""
+            '''
+            Split the string to two part
+            local name and domain name
+            '''
+            edit_string.append(i.rsplit("@"))           
+
+            '''
+            Edit a local name following the instructor
+            '''
+            for character in edit_string[index][0]:
+                if character == "+":
+                    break
+                if character != ".":
+                    str_temp += character
+
+            '''
+            Concatenate the email that already edited with domain name
+            '''
+            str_temp += "@" + edit_string[index][1]
+            # Add the email to the set #
+            emails_set.add(str_temp)
+            index+=1
+            
+        return len(emails_set)
